bd5ff508a5c201fb14e20005c39b501f87a5ff2b
WiPhil <philo102002@gmail.com>
2022-05-23 15:17:03 +0200

    feat : Download all files

---
+++ src/download.js
@@ -0,0 +1,33 @@
+const fs = require('fs-extra')
+const path = require('path')
+const util = require('util')
+const pump = util.promisify(require('pump'))
+
+const withStreamableFile = async (filePath, fn) => {
+  // creating empty file before streaming seems to fix some weird bugs with NFS
+  await fs.ensureFile(filePath + '.tmp')
+  await fn(fs.createWriteStream(filePath + '.tmp'))
+  // Try to prevent weird bug with NFS by forcing syncing file before reading it
+  const fd = await fs.open(filePath + '.tmp', 'r')
+  await fs.fsync(fd)
+  await fs.close(fd)
+  // write in tmp file then move it for a safer operation that doesn't create partial files
+  await fs.move(filePath + '.tmp', filePath, { overwrite: true })
+}
+
+module.exports = async (pluginConfig, dir = 'data', axios, log) => {
+  const datasetId = '5c34c4d1634f4173183a64f1'
+  const res = await axios.get('https://www.data.gouv.fr/api/1/datasets/' + datasetId)
+
+  const ressources = res.data.resources
+  for (const file of ressources) {
+    
+      await log.step(`téléchargement du fichier ${file.title}`)
+      const url = new URL(file.url)
+      const fileName = path.parse(url.pathname).base
+      await withStreamableFile(fileName, async (writeStream) => {
+        const res = await axios({ url: url.href, method: 'GET', responseType: 'stream' })
+        await pump(res.data, writeStream)
+      })
+  }
+}
