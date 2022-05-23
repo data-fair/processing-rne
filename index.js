24ee671004a02f9864a489b9531c9abce14a633a
WiPhil <philo102002@gmail.com>
2022-05-23 15:26:31 +0200

    feat : launch all process

---
+++ index.js
@@ -0,0 +1,11 @@
+const download = require('./src/download')
+const processData = require('./src/process')
+const upload = require('./src/upload')
+
+exports.run = async ({ pluginConfig, processingConfig, tmpDir, axios, log, patchConfig }) => {
+  
+  await download(pluginConfig, tmpDir, axios, log)
+  await processData(tmpDir, log)
+  if (!processingConfig.skipUpload) await upload(processingConfig, tmpDir, axios, log, patchConfig)
+  
+}
