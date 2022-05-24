6dbb6f55b34b740d7612d1ff47bb07eecac7f41d
WiPhil <philo102002@gmail.com>
2022-05-24 15:46:15 +0200

    fix : bug and log

--- src/download.js
+++ src/download.js
@@ -14,20 +14,22 @@ const withStreamableFile = async (filePath, fn) => {
   // write in tmp file then move it for a safer operation that doesn't create partial files
   await fs.move(filePath + '.tmp', filePath, { overwrite: true })
 }
-
-module.exports = async (pluginConfig, dir = 'data', axios, log) => {
+// processingConfig
+module.exports = async (dir = 'data', axios, log) => {
   const datasetId = '5c34c4d1634f4173183a64f1'
   const res = await axios.get('https://www.data.gouv.fr/api/1/datasets/' + datasetId + '/')
 
   const ressources = res.data.resources
+  console.log(ressources)
   await log.step('Téléchargement des fichiers')
   for (const file of ressources) {
-      const url = new URL(file.url)
-      const fileName = path.parse(url.pathname).base
-      const filePath = `${dir}/${fileName}`
-      await withStreamableFile(filePath, async (writeStream) => {
-        const res = await axios({ url: url.href, method: 'GET', responseType: 'stream' })
-        await pump(res.data, writeStream)
-      })
+    await log.info(`Téléchargement de ${file}`)
+    const url = new URL(file.url)
+    const fileName = path.parse(url.pathname).base
+    const filePath = `${dir}/${fileName}`
+    await withStreamableFile(filePath, async (writeStream) => {
+      const res = await axios({ url: url.href, method: 'GET', responseType: 'stream' })
+      await pump(res.data, writeStream)
+    })
   }
 }
