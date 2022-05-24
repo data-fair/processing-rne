58a9843b579f5c4f3d83a1d07292566c31e2c0ca
WiPhil <philo102002@gmail.com>
2022-05-24 15:47:26 +0200

    fix : formatting

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const processData = require('./src/process')
 const upload = require('./src/upload')
 
 exports.run = async ({ pluginConfig, processingConfig, tmpDir, axios, log, patchConfig }) => {
-  
   await download(pluginConfig, tmpDir, axios, log)
   await processData(tmpDir, log)
   if (!processingConfig.skipUpload) await upload(processingConfig, tmpDir, axios, log, patchConfig)
-  
 }
