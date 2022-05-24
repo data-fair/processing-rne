31d65a6f27e5c699ab13ea3471cc38456f43b282
WiPhil <philo102002@gmail.com>
2022-05-24 15:54:04 +0200

    fix : param method

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const download = require('./src/download')
 const processData = require('./src/process')
 const upload = require('./src/upload')
 
-exports.run = async ({ pluginConfig, processingConfig, tmpDir, axios, log, patchConfig }) => {
-  await download(pluginConfig, tmpDir, axios, log)
+exports.run = async ({ processingConfig, tmpDir, axios, log, patchConfig }) => {
+  await download(tmpDir, axios, log)
   await processData(tmpDir, log)
   if (!processingConfig.skipUpload) await upload(processingConfig, tmpDir, axios, log, patchConfig)
 }
